ac1dc59606c8d83d2d72c9fdf724e86415073a62
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-09-10 14:41:27 -0400

    amended code

--- server.js
+++ server.js
@@ -1,114 +1,21 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs")
+//This file contains all of the server implementation for the express server
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3001;
+//Include the express module and the routes
+const express = require('express');
+const PORT = process.env.PORT || 5001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-// Sets up the Express app to handle data parsing
+const app = express();
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-// HTML Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-
-// Include js files
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-// Use public folder
-app.use(express.static("public"));
-
-
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-
-});
-
-app.post("/api/notes", function (req, res) {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
-        if (error) {
-            return console.log(error)
-        }
-        notes = JSON.parse(notes)
-        
-        rewrite notes();
-        return res.status (200).end ();
-        
-        var id = notes[notes.length - 1].id + 1
-        var newNote = { title: req.body.title, text: req.body.text, id: id }
-        var activeNote = notes.concat(newNote)
-
-        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
-            if (error) {
-                return error
-            }
-            console.log(activeNote)
-            res.json(activeNote);
-        })
-    })
-})
-
-// Pull from db.json
-app.get("/api/notes", function (req, res) {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
-        if (error) {
-            return console.log(error)
-        }
-        console.log("This is Notes", data)
-        res.json(JSON.parse(data))
-    })
-});
-
-app.delete("/api/notes/:id", function (req, res) {
-    const noteId = JSON.parse(req.params.id)
-    console.log(noteId)
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
-        if (error) {
-            return console.log(error)
-        }
-        notes = JSON.parse(notes)
-
-        notes = notes.filter(val => val.id !== noteId)
-
-        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
-            if (error) {
-                return error
-            }
-            res.json(notes)
-        })
-    })
-})
-
-app.put("/api/notes/:id", function (req, res) {
-    const noteId = JSON.parse(req.params.id)
-    console.log(noteId)
-    fs.readFile(__dirname + "db/db.json", "utf8", function (error, notes) {
-        if (error) {
-            return console.log(error)
-        }
-        notes.JSONparse(notes)
-
-        notes = notes.filter(val => val.id !== noteId)
-
-        fs.writeFile(__dirname + "db/db.json", JSON.stringify(notes), function (error, data) {
-            if (error) {
-                return error
-            }
-            res.json(notes)
-        })
-    })
-})
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-    console.log('App listening on PORT: ${PORT}');
+//Listen on the defined port
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
