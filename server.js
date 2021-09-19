e9cb902d87ae10aa0176bab187ad91b42312256a
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-09-19 12:12:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 //Include the express module and the routes
 const express = require('express');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
