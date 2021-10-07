725f0c7ef36deb541c5ba6de35e47dce092885a4
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-07 12:32:54 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const router = require('express').Router();
 const ("express").Router();
-const {V4: uniqidv4} = require ("uniqid");
-const fs = require ("fs");
 
 
   let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
