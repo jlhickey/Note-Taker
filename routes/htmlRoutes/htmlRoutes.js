3742f0748e8bb1c30e122444025d80aead2690ed
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-07 12:29:51 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,14 +1,10 @@
 const path = require('path');
 const router = require('express').Router();
 const ("express").Router();
-const {V4: uniqidv4} =
-require ("uniqid");
+const {V4: uniqidv4} = require ("uniqid");
 const fs = require ("fs");
 
 
-//GET method route that will return notes.html when the path is "/notes"
-router.get("/notes", (req, res) => {
-  //creates variable from db.json file
   let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   //return notes
   return res.json(notes);
