0dd5911ca41ffa715a82f6d1102a9ebfb99bfcd5
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-07 12:07:51 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const router = require('express').Router();
+const ("express").Router();
+const {V4: uniqidv4} =
+require ("uniqid");
+const fs = require ("fs");
+
 
 //GET method route that will return notes.html when the path is "/notes"
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+router.get("/notes", (req, res) => {
+  //creates variable from db.json file
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  //return notes
+  return res.json(notes);
 });
 
-//GET method route that goes to index.html when any other path is called
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
 
 module.exports = router;
