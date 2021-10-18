743f8f79e796a0164a788541bc77697969f9fbd1
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-18 11:30:50 -0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -50,7 +50,7 @@ module.exports = app => {
         });
         
         // Display index.html when all other routes are accessed
-        app.get('*', function(req,res) {
+        app.get('/', function(req,res) {
             res.sendFile(path.join(__dirname, "../public/index.html"));
         });
 
