3a75d46c1b9579eb680bbd96a4a54607c73b8ddc
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-28 10:40:50 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,12 @@
-//HTML routes file which contains all the HTML method calls
+const path = require("path");
+const router = require("express").Router();
 
-//Include express and path modules
-const path = require('path');
-const router = require('express').Router();
-
-//GET method route that will return notes.html when the path is "/notes"
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./../public/notes.html"));
 });
 
-//GET method route that goes to index.html when any other path is called
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
