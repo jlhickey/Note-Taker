7ba0308ab53aac15ac55f86fb67771fbc8d6a514
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-28 11:00:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', htmlRoutes);
 
 // Listener
 app.listen(PORT, () => {
