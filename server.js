12551802a9b24281aeb864fb49cb1b701dbaf8b5
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 19:18:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-{
-  "name": "note-taker",
-  "version": "1.0.0",
-  "description": "",
-  "main": "server.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start": "server.js"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "express": "^4.16.4"
-  }
-}
+/ Dependencies
+// =============================================================
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// instantiate server
+const app = express();
+
+// create port
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsinng
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// public folder middleware for access to public folder CSS and JS
+app.use(express.static('public'));
+
+// use route folders
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Listener
+// =============================================================
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
