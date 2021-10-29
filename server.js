0b6b9f9ecde16fafa1e6028fe7970fcc636046fa
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 19:17:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-// Dependencies
+/ Dependencies
 // =============================================================
-const express = require("express");
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// instantiate server
 const app = express();
+
+// create port
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+// Sets up the Express app to handle data parsinng
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.json());
 
+// public folder middleware for access to public folder CSS and JS
+app.use(express.static('public'));
+
+// use route folders
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Listener
+// =============================================================
 app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+  console.log(`App listening on PORT ${PORT}`);
 });
