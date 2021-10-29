f558bdd192eead799e0bb50425626b27e6050d74
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 19:16:40 -0400

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,44 @@
-//API routes file which contains all the api method calls
+// Dependencies
+// =============================================================
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
-//Include the express and uniqid modules
-const router = require("express").Router();
-const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
+// API Routes
+// =============================================================
 
-router.get("/notes", (req, res) => {
+router.get('/notes', (req, res) => {
   //creates variable from db.json file
-  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
   //return notes to client
   return res.json(notes);
 });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
   let newNote = req.body;
-  // unique id via uuid package
+  //generate unique id with uuid package
   newNote.id = uuidv4();
   //create variable from db.json file
-  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  //push newNote to notes variable
+  let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
+  //push newNote to notes variable 
   notes.push(newNote);
   //write notes variable to db.json file
-  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  fs.writeFileSync('./data/db.json', JSON.stringify(notes));
   //return notes to client
   return res.json(notes);
 });
 
-router.delete("/notes/:id", (req, res) => {
-  //set file to delete
-  let deletedNote = req.params.id;
-  //creates arr from db.json file
-  let arr = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  //new arr without deleteFile
-  let newArr = arr.filter((file) => file.id !== deletedNote);
-  //rewrite json file with new arr
-  fs.writeFileSync("./db/db.json", JSON.stringify(newArr));
-  //return without deleted note
-  return res.json(newArr);
+router.delete('/notes/:id', (req, res) => {
+  //creates variable from file to delete
+  let deleteNote = req.params.id;
+  //creates variable from db.json file
+  let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
+  //removes/filters deleteFile from notes
+  let newNotes = notes.filter(file => file.id !== deleteNote);
+  //write notes variable to db.json file
+  fs.writeFileSync('./data/db.json', JSON.stringify(newNotes));
+  //return notes to client
+  return res.json(newNotes);
 });
 
 module.exports = router;
