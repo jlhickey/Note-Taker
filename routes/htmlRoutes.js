9d17b85e4e606b4545dc67741b662748ca44f800
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 12:45:36 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,11 +2,11 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname,  "../public/assets/notes.html"));
+  res.sendFile(path.join(__dirname, "./../public/assets/notes.html"));
 });
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/assets/index.html"));
 });
 
 module.exports = router;
