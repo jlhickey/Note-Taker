dc9f2491340862f53d4b4181f62dea157120ea0f
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 11:28:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-//This file contains all of the server implementation for the express server
-
-//Include the express module and the routes
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+const PORT = process.env.PORT || 3001;
 
-//Listen on the defined port
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`Listening on PORT ${PORT}`);
 });
