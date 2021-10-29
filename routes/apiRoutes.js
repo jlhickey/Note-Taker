8aa7d1f26f7b9dbe50be1d124108e64ed31f1411
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 19:37:59 -0400

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 
 router.get('/notes', (req, res) => {
   //creates variable from db.json file
-  let notes = JSON.parse(fs.readFileSync('./develop/db.json', 'utf8'));
+  let notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf8'));
   //return notes to client
   return res.json(notes);
 });
@@ -19,7 +19,7 @@ router.post('/notes', (req, res) => {
   //generate unique id with uuid package
   newNote.id = uuidv4();
   //create variable from db.json file
-  let notes = JSON.parse(fs.readFileSync('./develop/db.json', 'utf8'));
+  let notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf8'));
   //push newNote to notes variable 
   notes.push(newNote);
   //write notes variable to db.json file
@@ -32,7 +32,7 @@ router.delete('/notes/:id', (req, res) => {
   //creates variable from file to delete
   let deleteNote = req.params.id;
   //creates variable from db.json file
-  let notes = JSON.parse(fs.readFileSync('./develop/db.json', 'utf8'));
+  let notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf8'));
   //removes/filters deleteFile from notes
   let newNotes = notes.filter(file => file.id !== deleteNote);
   //write notes variable to db.json file
