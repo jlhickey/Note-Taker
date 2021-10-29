c410f38a729ee6a3014d49ece4527c0e2262b559
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-29 15:03:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Dependencies
+// =============================================================
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
